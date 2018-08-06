e4e86f13f8c718ce77411fe87b1aec3e8600e27a
kumbhanialex111 <kumbhani.bhavesh.1@gmail.com>
2018-08-06 17:17:03 +0530

    update for openshift

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 var express = require('express'),
     app = express();
 
-var port = process.env.PORT || 8080;
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
-var io = require('socket.io').listen(app.listen(port));
+// var port = process.env.PORT || 8080;
+
+var io = require('socket.io').listen(app.listen(server_port, server_ip_address));
 
 require('./config')(app, io);
 require('./routes')(app, io);
 
-console.log('Your application is running on http://localhost:' + port);
+console.log('Your application is running on http://localhost:' + server_port);
