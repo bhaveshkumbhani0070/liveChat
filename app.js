0e2fa6ca08c9d37aaad8672962dbc071b0235471
kumbhanialex111 <kumbhani.bhavesh.1@gmail.com>
2018-08-06 18:22:24 +0530

    Update for openshift

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var express = require('express'),
     app = express();
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || process.env.OPENSHIFT_INTERNAL_PORT || process.env.PORT || 3000;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || process.env.OPENSHIFT_INTERNAL_IP || 'localhost';
 
 // var port = process.env.PORT || 8080;
 
